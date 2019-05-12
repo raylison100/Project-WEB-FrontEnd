08946ef3a4096575b67c16ab4714945b9186483b
raylison nunes dos santos <raylison100@gmail.com>
2019-05-12 13:43:03 -0300

    Ajuste usermodel

--- src/app/services/api.integration.ts
+++ src/app/services/api.integration.ts
@@ -1,2 +1,2 @@
-// export const API_BACKEND: string = 'http://34.90.185.244:8082';
-export const API_BACKEND: string = 'http://localhost:8082';
+export const API_BACKEND: string = 'http://34.90.185.244:8082';
+// export const API_BACKEND: string = 'http://localhost:8082';
