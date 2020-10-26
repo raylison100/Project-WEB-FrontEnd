2ff2008850e2bf9f0e9fe81ac5f0f626f6b432ab
raylison100 <raylison100@gmail.com>
2020-10-26 15:11:46 -0300

    update project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   API_BACKEND     : 'http://127.0.0.1:8000/api/betterworld',
   grant_type      : "password",
   // client_secret   : "dh7ZrnnvgZTriPxNgeWcxwJnqy8YAtvqTfh6RFn1",
-  client_secret   : "Ozl4Sh8JrMCBtJLqh13ITIQ1KUj0guGF2Ne4cijv",
+  client_secret   : "xn3apdcNad9ffqCuAPr58OoR0TeTDMjWlFNdisvC",
 	client_id    	  : "2"
 };
 
